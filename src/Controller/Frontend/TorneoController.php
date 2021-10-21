be79ae6537f20142d2e4b2bc411f7ccdb274a04f
Martin Ibarra <mic@unixmexico.org>
2021-10-20 18:11:47 -0700

    added comunicacion, deportivo, torneo controllers

---
+++ src/Controller/Frontend/TorneoController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller\Frontend;
+
+use App\Entity\IcTraduccion;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TorneoController extends AbstractController
+{
+  
+    public function index(): Response
+    {
+        $article = new IcTraduccion();
+
+        if (!$this->isGranted('MANAGE', $article)) {
+
+            return $this->render('backend/admin/index.html.twig', [
+                'controller_name' => 'AdminController',
+            ]);
+
+        }
+    }
+}
